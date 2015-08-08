7299a96b96ec9e4fa842736d40ae280a3db7c073
Ranveer Aggarwal <ranveeraggarwal@gmail.com>
2015-08-08 19:33:10 +0530

    1a done

--- assignment1_GrayTrans/1/code/imageShrink.m
+++ assignment1_GrayTrans/1/code/imageShrink.m
@@ -2,14 +2,14 @@ function [out_im] = imageShrink(im, d)
 %Shrinks an image
 %   A rudimentary function to shrink an image
 [row, col] = size(im);
-out_im = zeros([uint8(row/d), uint8(col/d)]);
+%An image of size row/d, col/d
+out_im = cast(zeros([floor(row/d), floor(col/d)]),'like', im);
 [nrow, ncol] = size(out_im);
-
+%Subsampling via for loop
 for i = 1:nrow;
     for j = 1:ncol;
-        out_im(i,j) = im(uint8(i*d), uint8(j*d));
+        out_im(i,j) = im((i-1)*d+1, (j-1)*d+1);
     end
 end
-
 end
 
--- assignment1_GrayTrans/1/code/myMainScript.m
+++ assignment1_GrayTrans/1/code/myMainScript.m
@@ -1,13 +1,14 @@
 %% MyMainScript
-
 tic;
 %% Your code here
 curDir = pwd;
 
-%1a%
+%% 1(a)
 imgFil = fullfile(curDir, '..', 'data', 'circles_concentric.png');
+out_imgfil = fullfile(curDir, '..', 'images', 'circles_concentric_out.png');
 im = imread(imgFil);
-imshow(imageShrink(im, 5))
+imwrite(imageShrink(im, 2), out_imgfil);
 
-%1b%
+%% 1(b)
+
 toc;
