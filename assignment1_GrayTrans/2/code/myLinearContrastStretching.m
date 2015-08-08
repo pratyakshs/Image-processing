2537d00e2e036f1fec3a0bf9e223d8b446b27c58
Ranveer Aggarwal <ranveeraggarwal@gmail.com>
2015-08-08 20:27:47 +0530

    2a done

---
+++ assignment1_GrayTrans/2/code/myLinearContrastStretching.m
@@ -0,0 +1,15 @@
+function [out_img] = myLinearContrastStretching(in_img)
+%MYLINEARCONTRASTSTRETCHING Summary of this function goes here
+%   Detailed explanation goes here
+max_val = double(max(in_img));
+min_val = double(min(in_img));
+orig_range = max_val - min_val;
+[row, col] = size(in_img);
+out_img = zeros(row, col, 'like', in_img);
+
+for i = 1:row;
+    for j = 1:col;
+        out_img(i,j) = ((double(in_img(i,j))-min_val)/orig_range)*255.0;
+    end
+end
+end
--- assignment1_GrayTrans/2/code/myMainScript.m
+++ assignment1_GrayTrans/2/code/myMainScript.m
@@ -2,5 +2,12 @@
 
 tic;
 %% Your code here
+curDir = pwd;
+
+%% 2(a)
+imgFil = fullfile(curDir, '..', 'data', 'barbara.png');
+out_imgfil = fullfile(curDir, '..', 'images', '2a', 'barbara_out.png');
+im = imread(imgFil);
+imwrite(myLinearContrastStretching(im), out_imgfil);
 
 toc;
