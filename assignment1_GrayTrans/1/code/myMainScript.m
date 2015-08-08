3323e2a872fd899f52529dc59a5ea7e2b8e12ff1
Pratyaksh Sharma <pratyaksh@me.com>
2015-08-08 23:26:17 +0530

    Written main script for Shrink part

--- assignment1_GrayTrans/1/code/myMainScript.m
+++ assignment1_GrayTrans/1/code/myMainScript.m
@@ -4,11 +4,22 @@ tic;
 curDir = pwd;
 
 %% 1(a)
-imgFil = fullfile(curDir, '..', 'data', 'circles_concentric.png');
-out_imgfil = fullfile(curDir, '..', 'images', 'circles_concentric_out.png');
-im = imread(imgFil);
-imwrite(imageShrink(im, 2), out_imgfil);
+imgFile1 = fullfile(curDir, '..', 'data', 'circles_concentric.png');
+outImgFile1 = fullfile(curDir, '..', 'images', 'circles_concentric_shrinked-d-2.mat');
+outImgFile2 = fullfile(curDir, '..', 'images', 'circles_concentric_shrinked-d-3.mat');
+[img, map] = imread(imgFile1);
+iptsetpref('ImshowAxesVisible','on');
+figure('Name', 'Original circles_concentric.png'), imshow(img, map), colorbar, truesize;
+shrinked2 = myShrinkImageByFactorD(img, 2);
+figure('Name', 'Shrinked d=2 circles_concentric.png'), imshow(shrinked2, map), colorbar, truesize;
+save(outImgFile1, 'shrinked2');
+shrinked3 = myShrinkImageByFactorD(img, 3);
+figure('Name', 'Shrinked d=3 circles_concentric.png'), imshow(shrinked3, map), colorbar, truesize;
+save(outImgFile2, 'shrinked3');
+
 
 %% 1(b)
+
+%% 1(c)
 
 toc;
--- assignment1_GrayTrans/1/code/myNearestNeighborInterpolation.m
+++ assignment1_GrayTrans/1/code/myNearestNeighborInterpolation.m
@@ -1,13 +1,19 @@
 function [B] = myNearestNeighborInterpolation(A)
-[M, N] = size(A);
-B = zeros(3*M-2, 2*N-1);
-B(1, :) = nearestNeighborX(A(1, :));
-B(2, :) = B(1, :);
-for i = 2:M-1
-    B(3*(i-1), :) = nearestNeighborX(A(i, :));
+%% Resizes a (M, N) image matrix to a (3*M-2, 2*N-1) image.
+waitbar(0, 'Resizing using Nearest-Neighbor Interpolation');
+[M, N] = size(A);   % Dimension of input image
+B = zeros(3*M-2, 2*N-1);    % Empty output image
+B(1, :) = nearestNeighborX(A(1, :));    % First row of output image
+B(2, :) = B(1, :);  % Second row of output image
+wb = waitbar(1/M);  % Create a waitbar
+for i = 2:M-1   % For the rest of the input image rows
+    B(3*(i-1), :) = nearestNeighborX(A(i, :));  % Resize to 3 rows
     B(3*(i-1)+1, :) = B(3*(i-1), :);
     B(3*(i-1)+2, :) = B(3*(i-1), :);
+    waitbar(i/M);   % Update the waitbar
 end
-B(3*M-2, :) = nearestNeighborX(A(M, :));
+B(3*M-2, :) = nearestNeighborX(A(M, :));    % Last two rows of the output
 B(3*M-3, :) = B(3*M-2, :);
+waitbar(1);
+close(wb);  % Close the waitbar
 end
