e1fe965846e69ab8da483b9ef5e0a3c58e8bb4d9
Ranveer Aggarwal <ranveeraggarwal@gmail.com>
2015-08-27 00:04:34 +0530

    patch based filtering fucntion for a single pixel

--- assignment2_Filtering/3/code/myPatchHelper1.m
+++ assignment2_Filtering/3/code/myPatchHelper1.m
@@ -1,6 +1,6 @@
 function [ out_patch ] = myPatchHelper1( in_patch, gau_var )
-%UNTITLED2 Summary of this function goes here
-%   Detailed explanation goes here
+%Function to make the patch isotropic
+%  Done using a gaussian mask
 gau = fspecial('gaussian', size(in_patch), gau_var);
 out_patch = gau.*in_patch;
 end
--- assignment2_Filtering/3/code/patcher.m
+++ assignment2_Filtering/3/code/patcher.m
@@ -1,22 +1,34 @@
 function [ pix ] = patcher( ext_win, patch_size, win_size, h, gau_var )
-%UNTITLED4 Summary of this function goes here
-%   Detailed explanation goes here
+%This does the patch based filtering for one pixel with a window size
+%win_size around it
+%   ext_win is the extended window of size win_size+patch. This is because
+%   the boundary patches can go outside the original window size; h is the
+%   parameter to be tuned; gau_var is the gaussian variance used to make
+%   the patch isotropic; win_size is the original window size
     [rows, cols] = size(ext_win);
     pix_pos_row = ceil(rows/2);
     pix_pos_col = ceil(cols/2);
+    %pix_patch is the isotropic patch around the pixel for which the new
+    %intensity is to be calculated
     pix_patch = myPatchHelper1(ext_win(pix_pos_row-patch_size/2:pix_pos_row+patch_size/2,pix_pos_col-patch_size/2:pix_pos_col+patch_size/2), gau_var);
+    %Following are the limits for the loops
     i_init = floor((rows-win_size)/2 +1);
     i_final = floor((rows-win_size)/2 + 1 + win_size);
     j_init = floor((cols-win_size)/2 +1);
     j_final = floor((cols-win_size)/2 + 1 + win_size);
+    %This is the weight matrix wherein we store weights assigned on the
+    %basis of spatial distance
     weight_mat = zeros([win_size, win_size]);
+    %Iterating over every pixel in the window
     for i = i_init:i_final
         for j = j_init:j_final
             temp_patch = myPatchHelper1(ext_win(i-patch_size/2:i+patch_size/2,j-patch_size/2:j+patch_size/2), gau_var);
             weight_mat(i-i_init+1,j-j_init+1) = exp((-norm2(temp_patch - pix_patch))^2/h^2);
         end
     end
+    %We normalize the weight matrix
     weight_mat = weight_mat/sum(weight_mat(:));
+    %This is the weigted sum
     weight_sum = 0;
     for i = i_init:i_final
         for j = j_init:j_final
