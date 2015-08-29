7a8faffa88d6fff1c5cd3868a4f4db5a1dec8d23
Pratyaksh Sharma <pratyaksh@me.com>
2015-08-29 18:12:22 +0530

    Added waitbar for part 3

--- assignment2_Filtering/3/code/myPatchBasedFiltering.m
+++ assignment2_Filtering/3/code/myPatchBasedFiltering.m
@@ -3,11 +3,16 @@ function [ out_image ] = myPatchBasedFiltering( in_image, win_size, patch_size )
 %   Detailed explanation goes here
     [rows, cols] = size(in_image);
     fin_image = zeros(rows, cols);
+    waitbar(0, 'Applying patch-based filtering.');
+    wb = waitbar(0);
     for i = 1:rows
         for j = 1:cols
             fin_image(i,j) = patcher(in_image, [i,j], win_size, patch_size, 1, 1);
         end
+        waitbar(i/rows);
     end
+    waitbar(1);
+    close(wb);
     out_image = fin_image;
 end
 
