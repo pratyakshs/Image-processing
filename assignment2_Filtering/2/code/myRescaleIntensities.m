f36ebfafba0924b02a4e6d24ab7f785b3af0997b
Pratyaksh Sharma <pratyaksh@me.com>
2015-08-30 14:46:27 +0530

    Changed window

--- assignment2_Filtering/2/code/myRescaleIntensities.m
+++ assignment2_Filtering/2/code/myRescaleIntensities.m
@@ -1,7 +1,8 @@
 function [ imageRes ] = myRescaleIntensities(image, minI, maxI)
+%Function for linear contrast stretching
 %Rescales the intensities of input image to range [minI, maxI]
-minInp = min(image(:));
-Rinp = max(image(:)) - minInp;
-Rout = maxI - minI;
+minInp = min(image(:));  % Minimum intensity in input
+Rinp = max(image(:)) - minInp;  % Range of intensities in input
+Rout = maxI - minI;  % Range of intensitied in output
 imageRes = minI + ((image - minInp) * (Rout / Rinp));
 end
