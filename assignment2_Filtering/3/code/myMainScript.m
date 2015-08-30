194137065e7cad814604a7a301b919df057c678a
Pratyaksh Sharma <pratyaksh@me.com>
2015-08-30 16:42:03 +0530

    Published patch-based filtering report

--- assignment2_Filtering/3/code/myMainScript.m
+++ assignment2_Filtering/3/code/myMainScript.m
@@ -23,7 +23,12 @@ noisedImage = myNoisify(imageOrig);
 noisedImage = myRescaleIntensities(noisedImage, minOrigI, maxOrigI);
 
 %% Apply patch-based filtering
-im_out = myPatchBasedFiltering(noisedImage, 25, 9, 1);
+
+% Takes ~ 15 minutes to run, use saved .mat file instead
+% im_out = myPatchBasedFiltering(noisedImage, 25, 9, 1);
+inp_mat = fullfile(curDir, '..', 'data', 'barbaraFiltered.mat');
+load(inp_mat);
+
 % Optimal parameters are: h = 1, gau_var = 1
 % Window size = 25 and patch size = 9 were used.
 
@@ -33,14 +38,25 @@ gau = fspecial('gaussian', [patch_size patch_size], 1);
 gau = myRescaleIntensities(gau, minOrigI, maxOrigI);
 
 %% Optimal parameter values
-%h_opt = 1;
+% h_opt = 1;
 RMSD_opt = myRMSD(im_out, imageOrig)
 
 %% Display the images
+% Display the original image (subsampled)
 figure('Name', 'Original Barbara'), imshow(imageOrig, gray(100)), colorbar, truesize;
-figure('Name', 'Noisy Barbara'), figure, imshow(noisedImage, gray(100)), colorbar, truesize;
-figure('Name', 'Filtered Barbara'), figure, imshow(im_out, gray(100)), colorbar, truesize;
-figure('Name', 'Gaussian Mask'), imshow(gau, [minOrigI maxOrigI]), colorbar, truesize;  % display the noisy image
+title('Original Barbara')
+
+% Display the noise-added image
+figure('Name', 'Noisy Barbara'), imshow(noisedImage, gray(100)), colorbar, truesize;
+title('Noisy Barbara')
+
+% Display the filtered image
+figure('Name', 'Filtered Barbara'), imshow(im_out, gray(100)), colorbar, truesize;
+title('Filtered Barbara')
+
+% Display the Gaussian mask
+figure('Name', 'Gaussian Mask', 'Position', [0, 0, 500, 500]), imshow(gau, [minOrigI maxOrigI]), colorbar;
+title('Gaussian Mask')
 
 %% Save the images
 out_mat = fullfile(curDir, '..', 'data', 'barbaraFiltered.mat');
