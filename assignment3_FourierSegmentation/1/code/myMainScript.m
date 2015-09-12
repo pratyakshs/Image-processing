704c930dbf3188714359997183bc4f207d1e1782
Ranveer Aggarwal <ranveeraggarwal@gmail.com>
2015-09-12 23:19:15 +0530

    Initialize 1

--- assignment3_FourierSegmentation/1/code/myMainScript.m
+++ assignment3_FourierSegmentation/1/code/myMainScript.m
@@ -1,6 +1,21 @@
 %% MyMainScript
 
 tic;
-%% Your code here
+
+
+%% Setting global data
+curDir = pwd;
+
+%% Input files
+boat_file = fullfile(curDir, '..', 'data', 'boat.mat');
+
+%% Loading inputs
+load(boat_file);
+boatOrig = imageOrig;
+
+%% Display Images
+figure('Name', 'Boat Original'), 
+imshow(boatOrig, gray), colorbar, truesize;
+title('Boat Original');
 
 toc;
---
+++ assignment3_FourierSegmentation/1/code/myNoisify.m
@@ -0,0 +1,12 @@
+function [out_im] = myNoisify(in_im)
+% Adds noise to image
+%   Corrupts the image with independent and identically-distributed additive zero-mean Gaussian
+%   noise with standard deviation set to 10% of the intensity range.
+[rows, cols] = size(in_im);
+range = double(max(in_im(:))) - double(min(in_im(:)));
+stdev = (10*range)/100;
+dis = randn(rows, cols);
+noise = stdev*dis;
+size(noise);
+out_im = in_im + noise;
+end
