ccb54cc16538c74ef164c3cc8e85b23f892de0d6
Ranveer Aggarwal <ranveeraggarwal@gmail.com>
2015-09-13 18:44:48 +0530

    1 mostly done

--- assignment3_FourierSegmentation/1/code/myButterworthFiltering.m
+++ assignment3_FourierSegmentation/1/code/myButterworthFiltering.m
@@ -1,4 +1,4 @@
-function [ out_img ] = myButterworthFiltering( in_img, d0, n )
+function [ out_img, b_filter ] = myButterworthFiltering( in_img, d0, n )
 %Does Butterworth Filtering
 %   Detailed explanation goes here
     [rows, cols] = size(in_img);
@@ -9,5 +9,6 @@ function [ out_img ] = myButterworthFiltering( in_img, d0, n )
             h_matrix(i,j) = 1/(1 + (dist/d0)^(2*n));
         end
     end
+    b_filter = myRescaleIntensities(h_matrix, 0, 255);
     out_img = in_img.*h_matrix;
 end
--- assignment3_FourierSegmentation/1/code/myMainScript.m
+++ assignment3_FourierSegmentation/1/code/myMainScript.m
@@ -30,20 +30,20 @@ boatFreq = fft2(boatNois);
 %move the zero-frequency component to the center
 boatFreq = fftshift(boatFreq);
 %Apply Butterworth Filter
-boatTranF = myButterworthFiltering(boatFreq, dopt, nval);
+[boatTranF, b_filter] = myButterworthFiltering(boatFreq, dopt, nval);
 %reverse apply
-boatTranF = ifftshift(boatTranF);
-boatTran = ifft2(boatTranF);
+boatTranFS = ifftshift(boatTranF);
+boatTran = ifft2(boatTranFS);
 
 %% RMSD Values
 filtered_0 = myButterworthFiltering(boatFreq, dopt, nval);
-rmsd_0 = myRMSD(filtered_0, boatOrig)
+rmsd_0 = abs(myRMSD(filtered_0, boatOrig))/1000
 
 filtered_1 = myButterworthFiltering(boatFreq, dopt*0.95, nval);
-rmsd_1 = myRMSD(filtered_1, boatOrig)
+rmsd_1 = abs(myRMSD(filtered_1, boatOrig))/1000
 
 filtered_2 = myButterworthFiltering(boatFreq, dopt*1.05, nval);
-rmsd_2 = myRMSD(filtered_2, boatOrig)
+rmsd_2 = abs(myRMSD(filtered_2, boatOrig))/1000
 
 %% Display Images
 figure('Name', 'Boat Original'), 
@@ -55,7 +55,11 @@ imshow(boatNois, gray), colorbar, truesize;
 title('Boat Noised');
 
 figure('Name', 'Boat Transformed'), 
-imshow(boatTran, gray), colorbar, truesize;
+imshow(abs(boatTran), gray), colorbar, truesize;
 title('Boat Transformed');
+
+figure('Name', 'Butterworth Filter'), 
+imshow(b_filter, gray), colorbar, truesize;
+title('Butterworth Filter');
 
 toc;
