66247d99e11a6828d18d98d5132cf7544f159f69
Manik Dhar <dharmanik500@gmail.com>
2015-10-04 19:05:58 +0530

    Update myMainScriptATTFaces.m

--- assignment4_SVD_FaceRecognition/2/code/myMainScriptATTFaces.m
+++ assignment4_SVD_FaceRecognition/2/code/myMainScriptATTFaces.m
@@ -5,6 +5,7 @@ imageSize = 92 * 112;
 noOfppl = 40;
 trainImS = 5;
 testImS = 5;
+k_list = [1, 2, 3, 5, 10, 20, 30, 50, 75, 100, 125, 150, 170];
 tic;
-runRecognition(dataPath, imageSize, noOfppl, trainImS, testImS);
+runRecognition(dataPath, imageSize, noOfppl, trainImS, testImS, k_list);
 toc;
