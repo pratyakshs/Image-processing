467fdbf1317659c2fc201684b6d7b570fb85afdb
Pratyaksh Sharma <pratyaksh@me.com>
2015-10-24 07:21:29 +0530

    Simple Wiener Update done

--- assignment5_PCADenoising_Restoration_SVD/1/code/myMainScript.m
+++ assignment5_PCADenoising_Restoration_SVD/1/code/myMainScript.m
@@ -1,6 +1,18 @@
 %% MyMainScript
 
 tic;
-%% Your code here
+curDir = pwd;
+imgFile = fullfile(curDir, '..', 'data', 'barbara256.png');
+
+im = double(imread(imgFile));
+
+im1 = im + randn(size(im))*20;
+ 
+[im2] = myPCADenoising1(im1, 20);
+
+figure, imshow(im2/max(imRes(:)));
+figure, imshow(im/max(im(:)));
+
+%TODO calculate mean squared error b/w im2 and im
 
 toc;
---
+++ assignment5_PCADenoising_Restoration_SVD/1/code/myPCADenoising1.m
@@ -0,0 +1,72 @@
+function [imRes ] = myPCADenoising1( im, sigma )
+%MYPCADENOISING1 Summary of this function goes here
+%   Detailed explanation goes here
+
+P_x = size(im, 1) - 6;
+P_y = size(im, 2) - 6;
+N = P_x * P_y;
+
+P = zeros([49, N]);
+
+% Populate the matrix P using patches of size 7x7
+for i = 1:P_x
+    for j = 1:P_y
+        col_index = (i - 1) * P_y + j;
+        % Extract patch of size 7x7
+        patch = im(i:i+6, j:j+6);
+        % Reshape it and store in the correct column of P
+        P(:, col_index) = patch(:);
+    end
+end
+
+% Get eigenvectors of P*P'
+Q = P * P.';
+[V, lambda] = eig(Q);
+
+%Sort eigenvectors?
+%[lambda, ind] = sort(diag(lambda), 'descend');
+%lambda = diag(lambda);
+%V = V(:, ind);
+
+% And the eigen-coefficients
+A = V.' * P;
+
+% normalize?
+%for i=1:N
+%    A(:, i) = A(:, i)/norm(A(:, i));
+%end
+
+% Compute the estimates of the average squared eigen-coefficients 
+% of the original (clean) patches
+A_2 = A .* A;
+est = zeros([1 49]);
+for j = 1:49
+    est(j) = max(0, (sum(A_2(j, :))/N) - sigma^2);
+end
+
+% Compute the denoised coefficients
+denom = arrayfun(@(v) 1 + sigma^2/v, est);
+A_denoised = zeros(size(A));
+for j = 1:49
+    A_denoised(j, :) = A(j, :) / denom(j);
+end
+
+% Compute the denoised patches
+P_denoised = V * A_denoised;
+
+% Reconstruct the output image
+patch_add = zeros(size(im));
+patch_add_count = zeros(size(im));
+
+for i=1:P_x
+    for j=1:P_y
+        col_index = (i - 1) * P_y + j;
+        patch_add(i:i+6, j:j+6) = patch_add(i:i+6, j:j+6) + reshape(P_denoised(:, col_index), 7, 7);
+        patch_add_count(i:i+6, j:j+6) = patch_add_count(i:i+6, j:j+6) + 1;
+    end
+end
+
+imRes = patch_add ./ patch_add_count;
+
+end
+
